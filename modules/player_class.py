9aa5fc3edfcc91e7a07a29dd59e1a92d4054176a
maxhayes <maxmoney7pc@gmail.com>
2014-07-04 00:03:22 -0700

    grabbed working version

--- modules/player_class.py
+++ modules/player_class.py
@@ -20,7 +20,6 @@ from mxrydevtools import find_rotation
 class Player(pygame.sprite.Sprite):
     
     def __init__(self, image, start_x = 0, start_y = 0, 
-                 player_speed = [PLAYER_SPEED_a, PLAYER_SPEED_b], 
                  walking_speed = WALKING_SPEED,
                  recoil_speed = RECOIL_SPEED):
         
@@ -36,26 +35,14 @@ class Player(pygame.sprite.Sprite):
         self.hit_box = pygame.rect.Rect(0,0, self.hit_width, self.hit_height)
         self.hit_box.center = [start_x, start_y]
         # setup pos/speeds
-        # change hitbox size: !!!!!!!!!!!!!!!!
-        self.hit_box = pygame.Rect((self.rect.x + 60, self.rect.y + 30),
-                                    (self.rect.x + self.rect.width, self.rect.y + self.rect.height))
-        
-        # setup pos/speed
         self.rect.x = start_x
-        self.rect.y = start_y
-        self.pos_x = start_x
-        self.pos_y = start_y
-        
+        self.rect.y = start_y   
         self.walk_speed = walking_speed
         self.recoil_speed = recoil_speed
-        self.speed = player_speed
+        self.speed = PLAYER_SPEED
         self.changex, self.changey = 0,0
         
         self.moving = False
-        self.moving_left = False
-        self.moving_right = False
-        self.moving_up = False
-        self.moving_down = False
         self.player_fired = False
         self.laser_state = False
         
@@ -64,7 +51,7 @@ class Player(pygame.sprite.Sprite):
         #all_sprites_list.add(self)
         player_list.add(self)
         
-
+     
     # --- define class methods --- #   
     
     # increase fram counte each time its called, chooses framse from animations,
@@ -98,11 +85,6 @@ class Player(pygame.sprite.Sprite):
 
 
     # selects correct frame from feet walking animation
-    # --- define class methods --- #
-    
-
-    # heavy methods
-    
     def choose_feet_frame(self):
         self.feet_image = None
         if self.changex == 0 and self.changey == 0:
@@ -144,47 +126,6 @@ class Player(pygame.sprite.Sprite):
         # get center:
         self.pos_x, self.pos_y = self.rect.center
         # rotate images
-        
-        # -- calculate angle between player/mouse line and x = 0 --
-        # -- using Cos(A) = adj/hyp
-    
-    def rotate(self):
-        
-        # -- calculate angle between player/mouse line and x = 0 --
-        # -- using Cos(A) = adj/hyp
-        
-        # get mouse coord
-        (Mcoord) = pygame.mouse.get_pos()
-        Mx, My = Mcoord[0], Mcoord[1]
-        
-        # get player coord:
-        (Pcoord) = self.rect.center
-        Px, Py = Pcoord[0], Pcoord[1]
-        
-        
-        # find angle with cos(x) = adj/hyp
-        adj = Px - Mx
-        hyp = sqrt( pow(adj,2) + pow(   (Py - My)   ,2))
-        
-        if adj == 0:
-            adj += .00000000001
-            print('fixed divide by zero issue')
-        if hyp == 0:
-            hyp += .00000000001
-            print('fixed divide by zero issue')
-        raw_angle = degrees(acos(adj/hyp)) # 'raw_angle' does not account for quadrant
-        
-        # adjust for cursor being below x-axis of player's orgin
-        if Py > My:
-            self.angle = (180 - raw_angle) + 180
-        else:
-            self.angle = raw_angle
-        
-        # get center:
-        self.pos_x, self.pos_y = self.rect.center
-        
-        
-        # rotate image
         self.image = pygame.transform.rotate(self.image, self.angle)
         if self.feet_image:
             self.feet_image = pygame.transform.rotate(self.feet_image, self.angle)
@@ -201,6 +142,7 @@ class Player(pygame.sprite.Sprite):
             self.hit_box.height = self.hit_height
             self.hit_box.width = self.hit_width           
 #  
+    
     # draws the laser (figures out how long to make the line)
     def draw_laser(self):
         Mx, My = pygame.mouse.get_pos()
@@ -227,23 +169,6 @@ class Player(pygame.sprite.Sprite):
             Lx += delta_x*50
             Ly += delta_y*50
             # see if laser has moved off screen
-        delta_x = Mx - Px
-        delta_y = My - Py
-        
-        # corrects for some sort of uncaught 'divide by zero' error
-        if delta_x == 0: 
-            delta_x += .00000000001
-    
-        if delta_y == 0: 
-            delta_y += .00000000001
-        
-        while True:
-            # all_sprite_list collision detection will be here to stop laser @ objects
-            
-            
-            # if laser is still ending on screen
-            Lx += delta_x
-            Ly += delta_y
             if Lx in range(0,res_x):
                 pass
             elif Ly in range(0, res_y):
@@ -284,38 +209,22 @@ class Player(pygame.sprite.Sprite):
     def fire(self):
         gunshot_silenced.play()
         self.player_fired = True    
-    
-    # light methods
-    
-
     def moveup(self):
-        self.changey -= self.speed 
-        self.moving_up = True
+        self.changey -= self.speed
     def movedown(self):
-        self.changey += self.speed  
-        self.moving_down = True
+        self.changey += self.speed
     def moveleft(self):
         self.changex -= self.speed
-        self.moving_left = True
     def moveright(self):
         self.changex += self.speed
-        self.changex += self.speed
-        self.moving_right = True
-        
     def stopup(self):
         self.changey += self.speed
-        self.moving_up = False
     def stopdown(self):
         self.changey -= self.speed
-        self.moving_down = False
     def stopleft(self):
         self.changex +=self.speed
-        self.mobing_left = False
     def stopright(self):
         self.changex -= self.speed
-        self.changex -= self.speed
-        self.moving_right = False
-    
     def laser_on(self):
         self.laser_state = True
         laser_on.play()
@@ -327,58 +236,3 @@ class Player(pygame.sprite.Sprite):
             self.laser_off()
         else:
             self.laser_on()
-        
-    def fire(self):
-        gunshot_silenced.play()
-        self.player_fired = True
-        
-    def move_collide_rect(self):
-        # move up/down and collide check:
-        self.rect.x += self.changex
-        self.rect.y += self.changey
-        
-        player_hit_block = pygame.sprite.spritecollide(self, block_list, False)
-        for block in player_hit_block:
-            # make the smallest change possible to get player out of the wall
-            delta_left = block.rect.right - self.rect.left
-            delta_right = self.rect.right - block.rect.left
-            delta_top = block.rect.bottom - self.rect.top
-            delta_bottom = self.rect.bottom - block.rect.top
-            
-            # add all deltas to a list:
-            deltas = [delta_left, delta_right, delta_top, delta_bottom]
-            print(deltas)
-            print('%s: %s\n\n' % (deltas.index(min(deltas)), min(deltas)))
-            
-            if   deltas.index(min(deltas)) == 0: self.rect.x += min(deltas)
-            elif deltas.index(min(deltas)) == 1: self.rect.x -= min(deltas)
-            elif deltas.index(min(deltas)) == 2: self.rect.y += min(deltas)
-            elif deltas.index(min(deltas)) == 3: self.rect.y -= min(deltas)     
-            
-            
-        
-    def move(self):      
-        
-        
-        self.frame += 1
-        self.choose_feet_frame()
-        self.choose_body_frame()
-        self.rotate()
-        self.move_collide_rect()
-
-        
-    def update(self):
-        
-        self.move()
-        if self.moving:
-            screen.blit(self.feet_image, (self.rect.x, self.rect.y))
-        if self.laser_state:
-            self.draw_laser()
-            
-        # make hitbox visible for collision debug.
-        self.hit_box_vis = pygame.Surface([self.hit_box.width, self.hit_box.height])
-        self.hit_box_vis.fill([0,125,125,5])
-        screen.blit(self.hit_box_vis, (self.rect.x, self.rect.y))
-    
-        # draw player body
-        screen.blit(self.image, (self.rect.x, self.rect.y))
